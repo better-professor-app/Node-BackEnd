969f005f6689702dd72ba0a2ecafb82f61eab514
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-26 19:33:27 -0500

    GET /projects/student/123 Route Complete

--- projects/project-router.js
+++ projects/project-router.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+// DB Connection
 const Projects = require('./project-model')
+const Students = require('../students/student-model');
 
 router.get('/student/:id',  async (req, res) => {
+    loggedInId = req.loggedInId
     studentId = req.params.id
 
     try {
-        const projects = await Projects.getProjectByStudent(studentId)
+        const myStudent = await Students.getStudentById(loggedInId, studentId)
 
-        if(projects.length === 0) {
-            res.status(404).json({ message: "No Projects for Student"})
-        } else {
-            res.status(200).json(projects)
+        if (myStudent.length === 0) {
+            res.status(404).json({ message: `No Student With Id: ${studentId}`})
+        } 
+        else {
+
+            const projects = await Projects.getProjectByStudent(studentId)
+
+            if(projects.length === 0) {
+                res.status(404).json({ message: "No Projects for Student"})
+            } else {
+                res.status(200).json(projects)
+            }
+
         }
 
     } catch (err) {
