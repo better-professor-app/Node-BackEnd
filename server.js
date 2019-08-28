9625a2afaa8eeff0f4d8859b5410f8fcabaf8628
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-28 00:42:46 -0500

    Swagger Documentation Init

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 //routers
 const studentRouter = require('./students/student-router')
@@ -14,6 +16,31 @@ const authenticate = require('./auth/authenticate-middleware')
 //express
 const server = express();
 
+const swaggerDefinition = {
+    info: {
+      title: 'Better-Professor API',
+      version: '1.0.0',
+      description: 'Endpoints to access All Resources',
+    },
+    host: 'localhost:5500',
+    basePath: '/api',
+    securityDefinitions: {
+        JWT: {
+            type: 'apiKey',
+            name: 'Authorization',
+            description: 'JWT authorization of an API',
+            in: 'header',
+      },
+    },
+  };
+
+const options = {
+swaggerDefinition,
+apis: ['./*-router.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options)
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
@@ -22,6 +49,13 @@ server.use('/api/students', authenticate, studentRouter)
 server.use('/api/professors', professorRouter)
 server.use('/api/projects', authenticate, ProjectsRouter)
 server.use('/api/reminders', authenticate, ReminderRouter)
+
+server.get('/swagger.json', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+})
+
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 
 
