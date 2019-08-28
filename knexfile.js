d9b85762d4b703d02faa40b43dc6435f461b13cb
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-27 22:12:03 -0500

    Adding PG

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,13 @@
 // Update with your config settings.
 
+const localPg = {
+  host: 'localhost',
+  database: 'hobbits',
+  user: 'student',
+  password: 'hired'
+}
+const productionDBConnection = process.env.DATABASE_URL || localPg
+
 module.exports = {
 
   development: {
@@ -19,24 +27,20 @@ module.exports = {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done) //turns on the FK enforcement
       }
+    },
+    
+    production: {
+      client: 'pg',
+      connection: productionDBConnection,
+      migrations: {
+        directory: './database/migrations'
+      },
+      seeds: {
+        directory: './database/seeds'
+      },
     }
     
   }
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 
 };
